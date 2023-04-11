7abb0e765cb088b3069a7302b088ed7675631c12
pinomaker-hoo <inhoo23@naver.com>
2023-04-12 00:35:57 +0900

    feat : Set Swagger Config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import { Application } from 'express'
 import express from 'express'
 import 'reflect-metadata'
-import { createExpressServer } from 'routing-controllers'
+import {
+  createExpressServer,
+  getMetadataArgsStorage,
+} from 'routing-controllers'
+import { routingControllersToSpec } from 'routing-controllers-openapi'
+import { validationMetadatasToSchemas } from 'class-validator-jsonschema'
 
 // ** Util Imports
 import morgan from 'morgan'
@@ -12,6 +17,7 @@ import { logger, stream } from '@utils/logger'
 import compression from 'compression'
 import cookieParser from 'cookie-parser'
 import { connectDatabase } from '@databases'
+import * as swaggerUi from 'swagger-ui-express'
 
 // ** Controller Imports
 import CommonController from '@controllers/common.controller'
@@ -31,6 +37,37 @@ const expressApp: Application = createExpressServer({
     cookieParser(),
   ],
 })
+
+const storage = getMetadataArgsStorage()
+const schemas = validationMetadatasToSchemas({
+  refPointerPrefix: '#/components/schemas/',
+})
+
+const swaggerFile = routingControllersToSpec(
+  storage,
+  { routePrefix: '/api' },
+  {
+    components: {
+      schemas: schemas as any,
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+)
+
+swaggerFile.info = {
+  title: 'Express API',
+  version: '1.0.0',
+  description: 'Express API Docs',
+}
+
+expressApp.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 connectDatabase().catch((err) => {
   logger.error('Connect database error: ' + err)
